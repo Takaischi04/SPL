9eb59b74e1fe53bc5118ede06b8467633c323c7f
Tamir <tamir@htld>
2024-03-04 10:41:07 +0100

    SPL Python Wuerfelspiel 04.03.2024 Anfang

---
+++ Python Basics/Beispiele/Wuerfelspiel/Wuerfelspiel.py
@@ -0,0 +1,16 @@
+from random import randint
+
+# Erstelle ein Würfelspiel! Du spielst gegen den Computer. 
+# Wenn das Spiel startet (mit einem kleinen Menü), hat der Spieler 6 Würfe. 
+# Er spielt dabei gegen den Computer. Wenn die Augensumme höher ist als jene des 
+# Computers hat der Spieler gewonnen, ansonsten der Computer. 
+print("- - - - - - -")
+print("Würfelspiel")
+print("- - - - - - -")
+
+print("1. Cube -> 1 - 6")
+print("2. Dodecahedron -> 1 - 12")
+print("3. Octagonal bipyramid -> 1 - 16")
+print ("-")
+starting = int(input("Wähle die Würfelart:"))
+
