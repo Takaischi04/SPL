b812ce5b1a8ba8148bfcc16296db74ff4ddc1018
Tamir <tamir@htld>
2024-03-11 09:06:22 +0100

    SPL Python Wuerfelspiel 11.03.2024 fertig

--- Python Basics/Beispiele/Wuerfelspiel/Wuerfelspiel.py
+++ Python Basics/Beispiele/Wuerfelspiel/Wuerfelspiel.py
@@ -14,3 +14,67 @@ print("3. Octagonal bipyramid -> 1 - 16")
 print ("-")
 starting = int(input("Wähle die Würfelart:"))
 
+# Computer
+resultComputer = 0
+if starting == 1:
+    for diceVar in range(0, 6):
+        computerNormalDice = randint(1,6)
+        print("Rolls of computer: ", computerNormalDice)
+        resultComputer += computerNormalDice
+    print("-")
+    print("result of computer: ", resultComputer)
+    print("-")
+
+elif starting == 2:
+    for diceVar in range(0, 6):
+        computerDidecahedronDice = randint(1, 12)
+        print("Rolls of Computer: ", computerDidecahedronDice)
+        resultComputer += computerDidecahedronDice
+    print("-")
+    print("result of computer: ", resultComputer)
+    print("-")
+
+elif starting == 3:
+    for diceVar in range(0, 6):
+        computerOctagonalDice = randint(1, 16)
+        print("Rolls of computer", computerOctagonalDice)
+        resultComputer += computerOctagonalDice
+    print("-")
+    print("result of computer: ", resultComputer)
+    print("-")
+
+#Player
+resultPlayer = 0
+if starting == 1:
+    for diceVar in range(0, 6):
+        playerNormalDice = randint(1,6)
+        print("Rolls of player: ", playerNormalDice)
+        resultPlayer += playerNormalDice
+    print("-")
+    print("result of player: ", resultPlayer)
+    print("-")
+
+elif starting == 2:
+    for diceVar in range(0, 6):
+        playerDidecahedronDice = randint(1, 12)
+        print("Rolls of player: ", playerDidecahedronDice)
+        resultPlayer += playerDidecahedronDice
+    print("-")
+    print("result of player: ", resultPlayer)
+    print("-")
+
+elif starting == 3:
+    for diceVar in range(0, 6):
+        playerOctagonalDice = randint(1, 16)
+        print("Rolls of player", playerOctagonalDice)
+        resultPlayer += playerOctagonalDice
+    print("-")
+    print("result of player: ", resultPlayer)
+    print("-")
+
+if resultPlayer > resultComputer:
+    print("Game result: You win!")
+elif resultPlayer < resultComputer:
+    print("Game result: You lose!")
+elif resultPlayer == resultComputer:
+    print("Game result: draw!")
